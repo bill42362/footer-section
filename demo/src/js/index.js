70b45036ebd5fa6353944f9c564a050ab87b2224
Bill42362 <bill42362@gmail.com>
2017-08-10 18:35:41 +0800

    Enable starting express server by `npm start`.

--- demo/src/js/index.js
+++ demo/src/js/index.js
@@ -1,3 +1,31 @@
 // index.js
+'use strict';
+import Express from 'express';
 
-console.log('Hello');
+const WEB_PORT = process.env.PORT || 3000;
+const expressStaticRoutes = [
+    {path: '/fonts/', serverPath: '/dist/fonts'},
+    {path: '/img/', serverPath: '/dist/img'},
+    {path: '/css/', serverPath: '/dist/css'},
+    {path: '/js/', serverPath: '/dist/js'},
+];
+const renderApp = `
+    <!doctype html>
+    <html>
+        <head>
+            <title>footer-section</title>
+            <link rel="stylesheet" href="/css/index.css">
+        </head>
+        <body>
+            <div id="app-root"></div>
+            <script type='text/javascript' src="/js/index.js" ></script>
+        </body>
+    </html>
+`;
+const app = Express();
+
+app.get('/', (req, res) => { res.send(renderApp); })
+expressStaticRoutes.forEach(function(route) {
+    app.use(route.path, Express.static(__dirname + route.serverPath));
+});
+app.listen(WEB_PORT);
