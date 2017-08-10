50afd6223f8117919476c18a54c7f8a2879559bf
Bill42362 <bill42362@gmail.com>
2017-08-10 17:48:14 +0800

    Add basic files.

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+// index.js
+'use strict';
+import React from 'react';
+
+class FooterSection extends React.Component {
+    constructor(props) { super(props); }
+    render() {
+        return <div className='footer-section'></div>;
+    }
+}
+
+export default FooterSection;
