7165a6be223d720de1ad3b967691ce7c295f26bd
Bill42362 <bill42362@gmail.com>
2017-08-11 17:27:10 +0800

    Add data for demo.

--- demo/src/client/js/App.react.js
+++ demo/src/client/js/App.react.js
@@ -8,6 +8,41 @@ class App extends React.Component {
     render() {
         return <div className='app'>
             <FooterSection>
+                <a data-main_panel={true} data-logo={true} href='http://tw.pbplus.me'>
+                    <img data-image={true} src='/img/logo.svg' title='Logo' />
+                    <span data-title={true}>運動讓生活更有趣</span>
+                </a>
+                <a data-main_panel={true} data-app={true}>
+                    <img data-icon={true} src='/img/app-logo.svg' title='App Logo' />
+                    <span data-title={true}>pb+TV App立即下載</span>
+                </a>
+                <div data-links_panel={true} data-panel_key={0} data-panel_header={true}>
+                    <span data-title={true}>Services</span>
+                    <span data-description={true}>平台服務</span>
+                </div>
+                <a data-links_panel={true} data-panel_key={0} data-color='skyblue' href='http://facebook.com'>
+                    <span data-title={true}>TV直播</span>
+                    <span data-description={true}>運動賽事、pb+自製節目及更多活動精彩直播</span>
+                </a>
+                <a data-links_panel={true} data-panel_key={0}>
+                    <span data-title={true}>購票</span>
+                    <span data-description={true}>專屬運動迷的購票服務</span>
+                </a>
+                <div data-links_panel={true} data-panel_key={1} data-panel_header={true}>
+                    <span data-title={true}>Follow Us</span>
+                    <span data-description={true}>關注我們</span>
+                </div>
+                <a data-links_panel={true} data-panel_key={1}>
+                    <img data-icon={true} src='/img/facebook.svg' title='pb+運動平台 官方粉絲團' />
+                    <span data-title={true}>pb+運動平台 官方粉絲團</span>
+                </a>
+                <a data-links_panel={true} data-panel_key={1}>
+                    <img data-icon={true} src='/img/facebook.svg' title='揪in 官方粉絲團' />
+                    <span data-title={true}>揪in 官方粉絲團</span>
+                </a>
+                <a data-links_panel={true} data-panel_key={2}>
+                    <span data-title={true}>隱私權政策</span>
+                </a>
             </FooterSection>
         </div>;
     }
